ffae14449216ec9f3b5e17661d4b28a87ac3470b
Glatia <88858289+Glatia@users.noreply.github.com>
2024-05-14 14:38:38 -0700

    Update tictactoe.py

--- tictactoe.py
+++ tictactoe.py
@@ -1,6 +1,8 @@
-grid = [["#", "#", "#"],
-        ["#", "#", "#"], 
-        ["#", "#", "#"]]
+import numpy as np
+
+grid = np.array([["#", "#", "#"],
+                 ["#", "#", "#"], 
+                 ["#", "#", "#"]])
 
 running = True
 
@@ -14,8 +16,8 @@ def turn(whos_turn):
     x = int(coordsplit[0]) - 1
     y = int(coordsplit[1]) - 1
     
-    if grid[y][x] == '#':
-        grid[y][x] = whos_turn
+    if grid[y, x] == '#':
+        grid[y, x] = whos_turn
     else:
         print("Space already taken")
         returnGrid(grid)
@@ -28,24 +30,31 @@ def turn(whos_turn):
     
 def returnGrid(grid):
     
-    for y in grid:
-        print("|".join(y))
+    for y in range(len(grid)):
+        print("|".join(grid[y]))
+        if y < 2:
+            print("-+-+-")
 
 def checkGrid(grid, running):
+    
     for y in range(len(grid)):
+        row_check = np.unique(np.array([grid[y]]))
         
-        if len(set(grid[y])) == 1 and not set(grid[y]) == {'#'}:
+        if row_check.size == 1 and not row_check == '#':
             running = False
-            
-    for x in range(len(grid)):        
-        if len(set(i[x] for i in grid)) == 1 and not set(i[x] for i in grid) == {'#'}:
+
+    for x in range(len(grid)):
+        col_check = np.unique(np.array([i[x] for i in grid]))
+        if col_check.size == 1 and not col_check == '#':
             running = False
     
-    if len(set(grid[i][i] for i in range(len(grid)))) == 1 and not set(grid[i][i] for i in range(len(grid))) == {'#'}:
+    diag_check = np.unique(np.array([i[i] for i in range(grid.size)]))
+    if diag_check.size == 1 and not diag_check == '#':
         running = False
     
     inverted_grid = grid[::-1]
-    if len(set(inverted_grid[i][i] for i in range(len(inverted_grid)))) == 1 and not set(inverted_grid[i][i] for i in range(len(inverted_grid))) == {'#'}:
+    inverted_diag_check = np.unique(np.array([i][i] for i in range(inverted_grid.size)))
+    if inverted_diag_check.size == 1 and not inverted_diag_check == '#':
         running = False
     
     return running
